406ca06c6e102e2b53d2b09bac2760f0eb506041
Bharat Singh <10626464+Bharat1103@users.noreply.github.com>
2018-04-26 09:41:31 +0530

    Add logic to publish changes to gitbucket maven repos

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "gitbucket-mirroring-plugin"
 organization := "org.tmt"
 version := "1.0.0-SNAPSHOT"
 scalaVersion := "2.12.4"
-gitbucketVersion := "4.22.0"
+gitbucketVersion := "4.23.1"
 
 libraryDependencies ++= Seq()
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
--- src/main/scala/csw/tools/mirroring/service/GitService.scala
+++ src/main/scala/csw/tools/mirroring/service/GitService.scala
@@ -1,6 +1,6 @@
 package csw.tools.mirroring.service
 
-import java.io.File
+import java.io._
 import java.net.URI
 import java.util.Date
 
@@ -8,20 +8,36 @@ import csw.tools.mirroring.model.{Mirror, MirrorStatus, Repo}
 import gitbucket.core.util.Directory
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-import org.eclipse.jgit.transport.{FetchResult, RefSpec}
+import org.eclipse.jgit.transport.{RefSpec, URIish}
 import org.slf4j.LoggerFactory
 
+import scala.io.Source
 import scala.util.control.NonFatal
 
 class GitService(repo: Repo, mirror: Mirror) {
 
-  private val mirrorRefSpec  = new RefSpec("+refs/*:refs/*")
-  private val logger         = LoggerFactory.getLogger(classOf[MirrorService])
-  private val repositoryPath = s"${Directory.GitBucketHome}/repositories/${repo.owner}/${repo.name}.git"
+  private val mirrorRefSpec      = new RefSpec("+refs/*:refs/*")
+  private val fetchMirrorRefSpec = new RefSpec("refs/heads/*:refs/remotes/origin/*")
+  private val logger             = LoggerFactory.getLogger(classOf[MirrorService])
+  private val repositoryPath     = s"${Directory.GitBucketHome}/repositories/${repo.owner}/${repo.name}.git"
+
+  import scala.language.postfixOps
+
+  private val publishScriptPath = s"$repositoryPath/cloned/${repo.name}/publish.sh"
+  private val publishScriptContent =
+    s"""#!/bin/bash
+       |echo 'running publish script'
+       |cd $repositoryPath/cloned/${repo.name} && sbt publish""".stripMargin
+
+  private val cleanUpScriptPath = s"$repositoryPath/cloned/${repo.name}/delete.sh"
+  private val cleanUpScriptContent =
+    s"""#!/bin/bash 
+       |echo 'running clean-up script'
+       |rm -rf $repositoryPath/cloned""".stripMargin
 
   def sync(): Mirror = {
     val mirrorStatus = try {
-      fetch()
+      fetchAndPublish()
       successStatus()
     } catch {
       case NonFatal(ex) => failureStatus(ex)
@@ -30,13 +46,70 @@ class GitService(repo: Repo, mirror: Mirror) {
     mirror.withStatus(mirrorStatus)
   }
 
-  private def fetch(): FetchResult = {
-    val remoteUrl    = URI.create(mirror.remoteUrl)
-    val gitRepo      = git()
-    val fetchCommand = gitRepo.fetch().setRemote(remoteUrl.toString).setRefSpecs(mirrorRefSpec)
-    val result       = fetchCommand.call()
-    gitRepo.close()
-    result
+  def fetchAndPublish(): Unit = {
+    val remoteUrl     = URI.create(mirror.remoteUrl)
+    val gitRepo       = git()
+    val gitRepoRemote = gitRepo.remoteSetUrl()
+    gitRepoRemote.setName("origin")
+    gitRepoRemote.setUri(new URIish(remoteUrl.toString))
+    gitRepoRemote.call()
+    val initialMaster = Option(gitRepo.getRepository.resolve("master"))
+    initialMaster.fold {
+      gitRepo.fetch().setRemote(remoteUrl.toString).setRefSpecs(mirrorRefSpec).call()
+      publish()
+    } { master =>
+      gitRepo.fetch().setRemote(remoteUrl.toString).setRefSpecs(fetchMirrorRefSpec).call()
+      gitRepo.fetch().setRemote(remoteUrl.toString).setRefSpecs(mirrorRefSpec).call()
+      val newMasterOpt = Option(gitRepo.getRepository.resolve("master"))
+      newMasterOpt.foreach { newMaster =>
+        if (newMaster.compareTo(master) != 0) {
+          publish()
+        } else {
+          println("*" * 100)
+          println("No change detected, script artifacts are already up to date !!")
+          println("*" * 100)
+        }
+      }
+    }
+  }
+
+  private def publish(): Unit = {
+    println("*" * 100)
+    println("Changes detected, publishing script artifacts !!")
+    println("*" * 100)
+    // Cloning bare git repo to get the content
+    Git
+      .cloneRepository()
+      .setURI(repositoryPath)
+      .setDirectory(new File(s"$repositoryPath/cloned/${repo.name}/"))
+      .setBranch("refs/heads/master")
+      .call();
+    // Creating script for publishing artifacts
+    createScript(publishScriptPath, publishScriptContent)
+    // Running publish script
+    runScriptProcess(publishScriptPath)
+    // Creating script for deleting cloned repo - Tear down for next sync
+    createScript(cleanUpScriptPath, cleanUpScriptContent)
+    // Running tear down
+    runScriptProcess(cleanUpScriptPath)
+  }
+
+  private def createScript(path: String, content: String): Unit = {
+    val file   = new File(path)
+    val writer = new PrintWriter(file)
+    writer.write(content)
+    writer.close()
+    file.setExecutable(true, false)
+  }
+
+  private def runScriptProcess(scriptPath: String): Unit = {
+    val processBuilder: ProcessBuilder = new ProcessBuilder(scriptPath)
+    try {
+      val process = processBuilder.start();
+      Source.fromInputStream(process.getInputStream).getLines().foreach(println)
+    } catch {
+      case NonFatal(ex: IOException) => logger.error("Something went wrong !!", ex)
+    }
   }
 
   private def git(): Git = new Git(
